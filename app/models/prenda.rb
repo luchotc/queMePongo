c624f29af34376c08b0f89136d1423cc4fab38eb
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-05-13 20:35:18 -0300

    Making Prenda an ApplicationRecord

--- app/models/prenda.rb
+++ app/models/prenda.rb
@@ -1,14 +1,3 @@
-class Prenda
-  @@prendasRepository = [
-      {id: 1, tipo: "pantalon", talle: "SM"},
-      {id: 2, tipo: "saco", talle: "XL"}
-  ]
+class Prenda < ApplicationRecord
 
-  def self.all
-    @@prendasRepository
-  end
-
-  def self.find(id)
-    @@prendasRepository.find { |prenda| prenda[:id] == id }
-  end
 end
