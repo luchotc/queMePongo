7cee95efd5430748eabd88baf56454c983370c00
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-05-13 22:31:14 -0300

    Adding placeholders for controller

--- app/controllers/prendas_controller.rb
+++ app/controllers/prendas_controller.rb
@@ -6,4 +6,20 @@ class PrendasController < ApplicationController
   def show
     @prenda = Prenda.find(params[:id].to_i)
   end
+
+  def create
+    Prenda.create
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
 end
