782b8ca9778891c960619ce146390689eefbacce
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-05-27 17:06:30 -0300

    Adding missing templates

--- app/controllers/prendas_controller.rb
+++ app/controllers/prendas_controller.rb
@@ -1,25 +1,57 @@
 class PrendasController < ApplicationController
+  before_action :set_prenda, only: [:show, :edit, :update, :destroy]
+
+  # GET /prendas
   def index
     @prendas = Prenda.all
   end
 
+  # GET /prendas/1
   def show
-    @prenda = Prenda.find(params[:id].to_i)
   end
 
-  def create
-    Prenda.create
-  end
-
-  def update
-
-  end
-
+  # GET /prendas/new
   def new
-
+    @prenda = Prenda.new
   end
 
+  # GET /prendas/1/edit
   def edit
-
   end
+
+  # POST /prendas
+  def create
+    @prenda = Prenda.new(prenda_params)
+    if @prenda.save
+      redirect_to @prenda, notice: 'Prenda was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /prendas/1
+  def update
+    if @prenda.update(prenda_params)
+      redirect_to @prenda, notice: 'Prenda was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /prendas/1
+  def destroy
+    @prenda.destroy
+    redirect_to prendas_url, notice: 'Prenda was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_prenda
+      @prenda = Prenda.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def prenda_params
+      params.require(:prenda).permit(:name, :title, :context)
+    end
 end
