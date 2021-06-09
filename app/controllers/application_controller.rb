173a556750428f67a29de4b0715600472b66e381
Luis Cannavó <luistcannavo@gmail.com>
2021-06-09 00:43:38 -0300

    Remove unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :validate_logged_user!
-
-  def validate_logged_user!
-    if !current_user
-      redirect '/login'
-    end
-  end
-
   def current_user
     user_id = session[:user_id]
     if user_id
